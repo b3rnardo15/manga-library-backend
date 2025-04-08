79caa022b8617d7178be49b7ed0dc0a0336dae43
Bernardo Simões <151968699+b3rnardo15@users.noreply.github.com>
2025-04-07 22:37:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
