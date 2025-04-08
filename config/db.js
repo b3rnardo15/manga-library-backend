3ae4760171c35b7c8e9657922e25d34aeb722152
Bernardo Simões <151968699+b3rnardo15@users.noreply.github.com>
2025-04-07 22:36:18 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log('Tentando conectar com:', process.env.MONGO_URI); 
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     
     console.log(`✅ MongoDB conectado: ${conn.connection.host}`);
   } catch (err) {
     console.error(`❌ Erro ao conectar ao MongoDB: ${err.message}`);
+    console.error('String de conexão usada:', process.env.MONGO_URI); 
     process.exit(1);
   }
 };
